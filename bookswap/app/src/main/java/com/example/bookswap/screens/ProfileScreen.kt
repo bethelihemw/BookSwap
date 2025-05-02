fb4f1f2a8cb9799a04dd8278c2b4726d34355f0e
Natnael <n87294578@gmail.com>
2025-05-03 01:49:31 +0300

    Arranging the TextField and Background for Profile screen

--- bookswap/app/src/main/java/com/example/bookswap/screens/ProfileScreen.kt
+++ bookswap/app/src/main/java/com/example/bookswap/screens/ProfileScreen.kt
@@ -1,8 +1,6 @@
 package com.example.bookswap.screens
-mport android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
+import android.os.Bundle
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,7 +10,6 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.prof.ui.theme.ProfTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -35,10 +32,166 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-//import androidx.compose.material.icons.Icons
 
 
-@Composable
-fun ProfileScreen(onNavigateBack: () -> Unit) {
-    // Your profile screen UI here
+
+@Composable fun ProfileScreen() {
+    var name by remember { mutableStateOf("John Doe") }
+    val email = "john.doe@example.com"
+    var newPassword by remember { mutableStateOf("12345678") }
+    var showPassword by remember { mutableStateOf(false) }
+    var isEditing by remember { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(false) }
+    var showLogoutDialog by remember { mutableStateOf(false) }
+    var showDeleteDialog by remember { mutableStateOf(false) }
+
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        //background Image
+        Image(
+            painter = painterResource(id = R.drawable.contactBackground),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.height(40.dp))
+            Text(
+                text = "Profile",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            Spacer(Modifier.height(40.dp))
+
+            // Profile Icon
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Profile",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape),
+                tint = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(Modifier.height(40.dp))
+
+            // Username Field
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = isEditing,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    cursorColor = Color.Black
+                ),
+                singleLine = true,
+                leadingIcon = {
+                    IconButton(onClick = { showPassword = !showPassword }) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = "Profile"
+                        )
+                    }
+                }
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            // Email Field
+            TextField(
+                value = email,
+                onValueChange = {},
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = isEditing,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    cursorColor = Color.Black
+                ),
+                singleLine = true,
+                leadingIcon = {
+                    IconButton(onClick = { showPassword = !showPassword }) {
+                        Icon(
+                            imageVector = Icons.Default.Email,
+                            contentDescription = "Email"
+                        )
+                    }
+                }
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            //Password
+            TextField(
+                value = newPassword,
+                onValueChange = { newPassword = it },
+                label = { Text("New Password") },
+                enabled = isEditing,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    cursorColor = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent),
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password"
+                    )
+                },
+                trailingIcon = {
+                    IconButton(onClick = { showPassword = !showPassword }) {
+                        Icon(
+                            imageVector = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                            contentDescription = if (showPassword) "Hide Password" else "Show Password"
+                        )
+                    }
+                }
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+
+            // Loading Indicator
+            if (isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.5f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+
+        }
+    }
 }
