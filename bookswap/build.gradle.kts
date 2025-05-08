85dd9d14863fb580c308c20f7f8a329035b844c7
Tsedenya Bazezew <tsedenyabaze96@gmail.com>
2025-05-08 14:54:19 +0300

    Remove all existing content except README.md

--- bookswap/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false  // Add if using libraries
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false      // Add if using KAPT
-}
