e55bb557bfb42acd795865be5adeea1ebc037f33
Tsedenya Bazezew <tsedenyabaze96@gmail.com>
2025-05-09 03:04:08 +0300

    Book controller added

---
+++ backend/controller/bookController.js
@@ -0,0 +1,110 @@
+const Book = require("../models/books");
+const qrcode = require("qrcode")
+
+const addBook = async (req, res) => {
+  try {
+    const { title, author, genre, photo } = req.body;
+    const owner = req.user._id;
+
+    const newBook = new Book({
+      title,
+      author,
+      genre,
+      photo,
+      owner,
+    });
+
+    await newBook.save();
+
+    const qrcodeData = newBook._id.toString()
+    qrcode.toDataURL(qrcodeData, (err, url) =>{
+      if (err) {
+        console.error("Error generating QR code:", err);
+        return res.status(500).json({ error: "Failed to generate QR code" })
+    }
+    })
+
+     res.status(201).json({ book: newBook, qrCode: url });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+const getBooks = async (req, res) => {
+  try {
+    let {search, page, limit} = req.query;
+    let filter = {}
+    if(search !== undefined) filter.title = new RegExp(search, "i");
+    if (genre !== undefined) filter.genre = genre;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 5;
+    const skip = (page - 1) * limit;
+    const books = await Book.find(filter).skip(skip).limit(limit);
+    res.status(200).json({ Books: books });
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const getSingleBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    res.status(200).json({ Book: book });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.owner.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to update this book" });
+    }
+    const updatedBook = await Book.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({ message: "book updated successfully", updatedBook });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.owner.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete this book" });
+    }
+
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(204).send("book deleted successfully!");
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  addBook,
+  deleteBook,
+  update,
+  getBooks,
+  getSingleBook,
+};
